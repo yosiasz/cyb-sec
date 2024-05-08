03d4d554d6068327d7319a809345161ff45415f3
Josiah Solomon <yosiasz@gmail.com>
2024-05-08 13:40:01 -0700

    implement side nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 //import { useState } from 'react'
+//import LeftNav from "./components/LeftNav"
 
-import LeftNav from "./components/LeftNav"
+import ButtonAppBar from "./components/navigation/TopNavBar";
+import SideBarDrawer from "./components/navigation/SideBarDrawer";
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Header across the canvas</h1>
-      <LeftNav />
+      <ButtonAppBar />
+      <SideBarDrawer />
     </>
   )
 }
---
+++ client/src/components/navigation/SideBarDrawer.tsx
@@ -0,0 +1,59 @@
+//import * as React from 'react';
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+//function SideBarDrawer(_props: any) {
+function SideBarDrawer({ state, setState, toggleDrawer }) {
+
+  const toggleDrawerv = (newOpen: boolean) => () => {
+    setState(newOpen);
+  };
+  
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawerv(false)}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <Drawer open={state} onClose={toggleDrawerv(false)}>
+        {DrawerList}
+      </Drawer>
+    </>
+  );
+}
+
+export default SideBarDrawer;
